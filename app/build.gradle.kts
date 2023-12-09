8b293f36bbe12ab4382f511f1f28a18870953b8b
iggosha <iegolovkov@gmail.com>
2023-12-09 22:36:25 +0300

    Добавлен отчёт

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,6 @@ dependencies {
     val nav_version = "2.7.5"
     val room_version = "2.6.1"
     val retrofit_version = "2.9.0"
-    val glide_version = "4.16.0"
 
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
@@ -56,8 +55,6 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
     implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
-
-    implementation("com.github.bumptech.glide:glide:$glide_version")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
